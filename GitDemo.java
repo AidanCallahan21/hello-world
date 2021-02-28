6143191a21b6c11b3870cfe7c32282914681c5a7
AidanCallahan21 <79811250+AidanCallahan21@users.noreply.github.com>
2021-02-28 15:02:20 -0700

    Update GitDemo.java
    I added my name on line 2
    I modified the addNumber function so it adds num and previouNumber together
    I added a new variable named reverseNum on line 7 and added a statement on line 49 in the reverseNum function

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Aidan Callahan (#1214845281)
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -12,9 +13,9 @@ public class GitDemo {
     /*****
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
-    public double addNumber(double num) {
+    public double addNumber(double num, double previousNumber) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        return num + previousNumber;
     }
     
     /*****
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
